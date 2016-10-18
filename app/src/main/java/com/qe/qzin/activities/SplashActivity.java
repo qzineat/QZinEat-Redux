04f3f2c2e1c9ea19759eadda5f8a653b612603c0
Shyam Rokde <shyam.rokde@gmail.com>
2016-10-17 17:17:12 -0700

    Splash

--- app/src/main/java/com/qe/qzin/activities/SplashActivity.java
+++ app/src/main/java/com/qe/qzin/activities/SplashActivity.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.qe.qzin.R;
+import com.squareup.picasso.Picasso;
 
 public class SplashActivity extends AppCompatActivity {
 
   private final int SPLASH_DISPLAY_LENGTH = 2500;
+
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,15 @@ public class SplashActivity extends AppCompatActivity {
     getWindow().getDecorView().setSystemUiVisibility(
         View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+    ImageView ivSplashBg = (ImageView) findViewById(R.id.ivSplashBg);
+
+    Picasso.with(getApplicationContext())
+        .load(R.drawable.feast)
+        .fit()
+        .centerCrop()
+        .into(ivSplashBg);
+
 
     // TODO: We should call REST API  and load data before loading main activity
     new Handler().postDelayed(new Runnable() {
