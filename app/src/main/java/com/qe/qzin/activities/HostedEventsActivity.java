d5447816fecf65b739ea49aa0246aaaa0ed322a3
Rokde, Ghanshyam <srokde@visa.com>
2017-03-03 22:15:20 -0800

    New activity for listing hosted events
    #25

--- app/src/main/java/com/qe/qzin/activities/HostedEventsActivity.java
+++ app/src/main/java/com/qe/qzin/activities/HostedEventsActivity.java
@@ -1,11 +1,10 @@
 package com.qe.qzin.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.qe.qzin.R;
 
-public class HostedEventsActivity extends AppCompatActivity {
+public class HostedEventsActivity extends BaseActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
