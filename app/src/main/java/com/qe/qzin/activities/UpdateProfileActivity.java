1818647f6a2b7fa187370b91205ff717fd68db71
Rokde, Ghanshyam <srokde@visa.com>
2017-03-11 11:40:10 -0800

    user profile #7

---
+++ app/src/main/java/com/qe/qzin/activities/UpdateProfileActivity.java
@@ -0,0 +1,14 @@
+package com.qe.qzin.activities;
+
+import android.os.Bundle;
+
+import com.qe.qzin.R;
+
+public class UpdateProfileActivity extends BaseActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_update_profile);
+  }
+}
